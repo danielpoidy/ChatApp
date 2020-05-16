70c870205390724439258c3137d6e2f3de8d235b
Dangeecko <poidydaniel@gmail.com>
2020-05-16 18:48:12 +0000

    Création de la page de connexion

---
+++ src/components/Connexion.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Connexion extends Component {
+    render() {
+        return (
+            <div className='connexionBox'>
+                <form className='connexion'>
+                <input
+                type='text'
+                    required
+                />
+                <button 
+                type="submit">GO</button>
+
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Connexion
